f56b3d7fd7f890402b0f5a1e6158547a382bc8b6
Wassim Benzarti <wass11121996@gmail.com>
2020-02-26 11:16:04 +0100

    Added changes

---
+++ project/src/main/java/insat/App.java
@@ -0,0 +1,17 @@
+package insat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    /**
+     * The entrypoint of the application
+     * @param args
+     */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
